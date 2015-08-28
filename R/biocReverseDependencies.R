acf1506155202d98b753b7e41847db3a4ccb2224
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-08-28 10:45:15 +0100

    Fixed magrittr problem by adding import in NAMESPACE

--- R/biocReverseDependencies.R
+++ R/biocReverseDependencies.R
@@ -1,4 +1,15 @@
 
+#' Title
+#'
+#' @param pkg The name of the bioconductor package
+#'
+#' @return The total number of reverse dependencies
+#' @import rvest
+#' @import magrittr
+#' @export
+#'
+#' @examples
+#' getRevDeps("S4Vectors")
 getRevDeps <- function(pkg) {
   
   
@@ -7,21 +18,21 @@ getRevDeps <- function(pkg) {
   
   # Pull the html
   pkgUrl <- paste0("http://www.bioconductor.org/packages/release/bioc/html/", pkg, ".html")
-  bio <- rvest::html(pkgUrl)
+  bio <- html(pkgUrl)
   
   # Extract the rows of the details table into a list
-  bio %>% rvest::html_node("table.details") %>% rvest::html_nodes("tr") -> biotr
+  bio %>% html_node("table.details") %>% html_nodes("tr") -> biotr
   
   # Function to count the number of hyper links in a cell
   getNAnchors <- function(x) {
-    td <- x %>% rvest::html_nodes("td")
-    return(td[[2]] %>% rvest::html_nodes("a") %>% length)
+    td <- x %>% html_nodes("td")
+    return(td[[2]] %>% html_nodes("a") %>% length)
   }
   
   # Function to extract the name from a cell
   tdName <- function(x) {
-    td <- x %>% rvest::html_nodes("td")
-    return(td[[1]] %>% rvest::html_text)
+    td <- x %>% html_nodes("td")
+    return(td[[1]] %>% html_text)
   }
   
   # Extract text from left hand column
@@ -36,7 +47,7 @@ getRevDeps <- function(pkg) {
   #cat("Total Reverse Dependencies:", nRevDeps)
   
   biotr[which(field %in% c("Depends On Me", "Imports Me"))] %>% 
-    rvest::html_nodes("a") %>% rvest::html_text %>% unique %>% length -> nRevDeps
+    html_nodes("a") %>% html_text %>% unique %>% length -> nRevDeps
   
   return(nRevDeps)
   
