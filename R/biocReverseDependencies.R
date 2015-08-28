f3f846420f28529dfd739b6d7a751a679a2c25ca
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-08-28 10:24:19 +0100

    added rvest namespace flags

--- R/biocReverseDependencies.R
+++ R/biocReverseDependencies.R
@@ -7,21 +7,21 @@ getRevDeps <- function(pkg) {
   
   # Pull the html
   pkgUrl <- paste0("http://www.bioconductor.org/packages/release/bioc/html/", pkg, ".html")
-  bio <- html(pkgUrl)
+  bio <- rvest::html(pkgUrl)
   
   # Extract the rows of the details table into a list
-  bio %>% html_node("table.details") %>% html_nodes("tr") -> biotr
+  bio %>% rvest::html_node("table.details") %>% rvest::html_nodes("tr") -> biotr
   
   # Function to count the number of hyper links in a cell
   getNAnchors <- function(x) {
-    td <- x %>% html_nodes("td")
-    return(td[[2]] %>% html_nodes("a") %>% length)
+    td <- x %>% rvest::html_nodes("td")
+    return(td[[2]] %>% rvest::html_nodes("a") %>% length)
   }
   
   # Function to extract the name from a cell
   tdName <- function(x) {
-    td <- x %>% html_nodes("td")
-    return(td[[1]] %>% html_text)
+    td <- x %>% rvest::html_nodes("td")
+    return(td[[1]] %>% rvest::html_text)
   }
   
   # Extract text from left hand column
@@ -36,7 +36,7 @@ getRevDeps <- function(pkg) {
   #cat("Total Reverse Dependencies:", nRevDeps)
   
   biotr[which(field %in% c("Depends On Me", "Imports Me"))] %>% 
-    html_nodes("a") %>% html_text %>% unique %>% length -> nRevDeps
+    rvest::html_nodes("a") %>% rvest::html_text %>% unique %>% length -> nRevDeps
   
   return(nRevDeps)
   
